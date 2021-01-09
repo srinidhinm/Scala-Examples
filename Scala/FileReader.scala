a28c22fde36896b6686c078866d3c6713917a1af
srinidhinm <srinibhat1988@gmail.com>
2021-01-09 12:13:56 +0530

    Initial Commit for FileReader

---
+++ Scala/FileReader.scala
@@ -0,0 +1,14 @@
+import scala.io.Source
+import java.io.{FileNotFoundException, IOException}
+
+object FileReader extends App {
+    val filename = "no-such-file.scala"
+    try {
+        for (line <- Source.fromFile(filename).getLines) {
+            println(line)
+        }
+    } catch {
+        case e: FileNotFoundException => println("Couldn't find that file ["+filename+"]")
+        case e: IOException => println("Got an IOException!")
+    }
+}
