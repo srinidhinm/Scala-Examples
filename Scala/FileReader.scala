b6b4958a4e1b2c6e5c6e22858a7198a0064c2cd9
srinidhinm <srinibhat1988@gmail.com>
2021-01-09 13:47:04 +0530

    Commit Changes with Error Handling

--- Scala/FileReader.scala
+++ Scala/FileReader.scala
@@ -1,14 +1,49 @@
 import scala.io.Source
 import java.io.{FileNotFoundException, IOException}
 
-object FileReader extends App {
-    val filename = "no-such-file.scala"
-    try {
-        for (line <- Source.fromFile(filename).getLines) {
-            println(line)
+/**
+    This is a Scala Example Program to Read a File and print its contents and also handle error cases
+    Case 1:
+        a. Ensure You Create a File names 'Cities.txt' in the working Directory
+        b. Contents of the File Should be Displayed on Console
+    Case 2:
+        a. Ensure 'Invalid.txt' doesn't exists in working directory
+        b. A Error Message 'Couldn't find that file [Invalid.txt]' should be displayed on the console
+
+    How to Run:
+        1. Create a Cities.txt file with following Contents
+            Bangalore
+            Mysore
+            Chennai
+        2. Compile using command 'scalac FileReader.scala'
+        3. Run 'scala FileReader Cities.txt'
+            Output:
+                Bangalore
+                Mysore
+                Chennai
+        4. Run 'scala FileReader Invalid.txt'
+            Output:
+                'Couldn't find that file [Invalid.txt]'
+*/
+
+class FileReader extends App {
+    def readandPrintFile(fileName:String) ={
+        try {
+            val validFile = Source.fromFile(fileName);
+            for (line <- validFile.getLines) {
+                println(line)
+            }
+        } catch {
+            case e: FileNotFoundException => println("Couldn't find that file ["+fileName+"]")
+            case e: IOException => println("Got an IOException!")
         }
-    } catch {
-        case e: FileNotFoundException => println("Couldn't find that file ["+filename+"]")
-        case e: IOException => println("Got an IOException!")
     }
-} 
+}
+
+object Reader{
+    def main(args: Array[String]){
+        val fileName =  new FileReader();
+        fileName.readandPrintFile(args(0))
+    }
+}
+    
